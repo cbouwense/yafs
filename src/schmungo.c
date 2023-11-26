f59a75e58a06b7923efc4bffa98024c37eac2d75
Christian Bouwense <bouwensec@gmail.com>
2023-11-25 20:17:06 -0500

    adding in debug stuff

--- src/schmungo.c
+++ src/schmungo.c
@@ -28,29 +28,16 @@
 #define N (1<<13)
 #define FONT_SIZE 64
 
-#define RENDER_FPS 30
+#define COLOR_BACKGROUND BLACK
 #define RENDER_FACTOR 100
 #define RENDER_WIDTH (16*RENDER_FACTOR)
 #define RENDER_HEIGHT (9*RENDER_FACTOR)
 
-#define COLOR_ACCENT                  ColorFromHSV(225, 0.75, 0.8)
-#define COLOR_BACKGROUND              GetColor(0x151515FF)
-#define COLOR_TRACK_PANEL_BACKGROUND  ColorBrightness(COLOR_BACKGROUND, -0.1)
-#define COLOR_TRACK_BUTTON_BACKGROUND ColorBrightness(COLOR_BACKGROUND, 0.15)
-#define COLOR_TRACK_BUTTON_HOVEROVER  ColorBrightness(COLOR_TRACK_BUTTON_BACKGROUND, 0.15)
-#define COLOR_TRACK_BUTTON_SELECTED   COLOR_ACCENT
-#define COLOR_TIMELINE_CURSOR         COLOR_ACCENT
-#define COLOR_TIMELINE_BACKGROUND     ColorBrightness(COLOR_BACKGROUND, -0.3)
-#define COLOR_HUD_BUTTON_BACKGROUND   COLOR_TRACK_BUTTON_BACKGROUND
-#define COLOR_HUD_BUTTON_HOVEROVER    COLOR_TRACK_BUTTON_HOVEROVER
-
-#define HUD_TIMER_SECS    1.0f
-#define HUD_BUTTON_SIZE   50
-#define HUD_BUTTON_MARGIN 50
-#define HUD_ICON_SCALE    0.5
-
 typedef struct GameState {
     Font font;
+
+    bool debug_mode;
+    bool paused;
 
     int player_one_score;
     int player_two_score;
@@ -71,35 +58,35 @@ typedef struct Paddle {
 
 // CSS-like helpers --------------------------------------------------------------------------------
 
-float sch_vh(float vh) {
+float vh(float vh) {
     return GetRenderHeight() * vh * 0.01;
 }
 
-float sch_vw(float vw) {
+float vw(float vw) {
     return GetRenderWidth() * vw * 0.01;
 }
 
-float sch_x_center(float width) {
+float x_center(float width) {
     return GetRenderWidth() / 2 - width / 2;
 }
 
-float sch_y_center(float height) {
+float y_center(float height) {
     return GetRenderHeight() / 2 - height / 2;
 }
 
-float sch_top() {
+float top() {
     return 0;
 }
 
-float sch_right() {
+float right() {
     return GetRenderWidth();
 }
 
-float sch_bottom() {
+float bottom() {
     return GetRenderHeight();
 }
 
-float sch_left() {
+float left() {
     return 0;
 }
 
@@ -113,9 +100,9 @@ int main(void)
     InitAudioDevice();
 
     GameState *game_state;
-    Ball *ball;
-    Paddle *paddle_one;
-    Paddle *paddle_two;
+    Ball      *ball;
+    Paddle    *paddle_one;
+    Paddle    *paddle_two;
 
     { // Initialize the game state
         game_state = malloc(sizeof(*game_state));
@@ -123,6 +110,7 @@ int main(void)
         memset(game_state, 0, sizeof(*game_state));
 
         game_state->font = LoadFontEx("resources/fonts/Alegreya-Regular.ttf", FONT_SIZE, 0, 0);
+        game_state->debug_mode = false;
         game_state->player_one_score = 0;
         game_state->player_two_score = 0;
     }
@@ -132,7 +120,7 @@ int main(void)
         assert(ball != NULL && "Buy more RAM lol");
         memset(ball, 0, sizeof(*ball));
 
-        ball->position = (Vector2) { sch_x_center(25), sch_y_center(25) };
+        ball->position = (Vector2) { x_center(25), y_center(25) };
         ball->velocity = (Vector2) { 1, 0 };
     }
 
@@ -141,28 +129,28 @@ int main(void)
         assert(paddle_one != NULL && "Buy more RAM lol");
         memset(paddle_one, 0, sizeof(*paddle_one));
 
-        paddle_one->position = (Vector2) { sch_vw(10), sch_vh(50) };
+        paddle_one->position = (Vector2) { vw(10), vh(50) };
 
         paddle_two = malloc(sizeof(*paddle_two));
         assert(paddle_two != NULL && "Buy more RAM lol");
         memset(paddle_two, 0, sizeof(*paddle_two));
 
-        paddle_two->position = (Vector2) { sch_vw(90), sch_vh(50) };
+        paddle_two->position = (Vector2) { vw(90), vh(50) };
     }
 
     while (!WindowShouldClose()) {
         { // Update
-            { // Update ball
+            if (!game_state->paused) { // Update ball
                 ball->position.x += ball->velocity.x;
                 ball->position.y += ball->velocity.y;
             }
 
-            { // Update paddles
+            if (!game_state->paused) { // Update paddles
                 paddle_one->position.y = GetMousePosition().y;
                 paddle_two->position.y = GetMousePosition().y;
             }
 
-            { // Check for collisions
+            if (!game_state->paused) { // Check for collisions
                 // Check for collisions with the top and bottom of the screen
                 if (ball->position.y <= 0 || ball->position.y >= GetRenderHeight()) {
                     ball->velocity.y *= -1;
@@ -180,14 +168,24 @@ int main(void)
                 // Check for collisions with the left and right of the screen
                 if (ball->position.x <= 0) {
                     game_state->player_two_score++;
-                    ball->position = (Vector2) { sch_x_center(25), sch_y_center(25) };
+                    ball->position = (Vector2) { x_center(25), y_center(25) };
                     ball->velocity = (Vector2) { 0, 0 };
                 }
 
                 if (ball->position.x >= GetRenderWidth()) {
                     game_state->player_one_score++;
-                    ball->position = (Vector2) { sch_x_center(25), sch_y_center(25) };
+                    ball->position = (Vector2) { x_center(25), y_center(25) };
                     ball->velocity = (Vector2) { 0, 0 };
+                }
+            }
+        
+            { // Update game state
+                if (IsKeyPressed(KEY_F1)) {
+                    game_state->debug_mode = !game_state->debug_mode;
+                }
+
+                if (IsKeyPressed(KEY_SPACE)) {
+                    game_state->paused = !game_state->paused;
                 }
             }
         }
@@ -196,16 +194,37 @@ int main(void)
             BeginDrawing();
             ClearBackground(COLOR_BACKGROUND);
 
-            // { // Draw grid
-            //     for (int i = 0; i < 20; i++) {
-            //         DrawRectangle(sch_vw(5 * i), 0, 2, sch_vh(100), GREEN);
-            //         DrawRectangle(0, sch_vh(5 * i), sch_vw(100), 2, GREEN);
-            //     }
-            // }
+            // Draw grid
+            if (game_state->debug_mode) {
+                for (int i = 0; i < 20; i++) {
+                    DrawRectangle(vw(5 * i), 0, 1, vh(100), GREEN);
+                    DrawRectangle(0, vh(5 * i), vw(100), 1, GREEN);
+                }
+            }
 
-            // Draw scores
-            DrawTextEx(game_state->font, TextFormat("%i", game_state->player_one_score), (Vector2){ sch_vw(40), sch_vh(5) }, FONT_SIZE, 0, WHITE);
-            DrawTextEx(game_state->font, TextFormat("%i", game_state->player_two_score), (Vector2){ sch_vw(60), sch_vh(5) }, FONT_SIZE, 0, WHITE);
+            { // Draw player one score
+                const Vector2 score_one_position = (Vector2) { vw(40), vh(5) };
+                const char *score_one_text = TextFormat("%i", game_state->player_one_score);
+                DrawTextEx(game_state->font, score_one_text, score_one_position, FONT_SIZE, 0, WHITE);
+                
+                // Draw player one score outline
+                if (game_state->debug_mode) {
+                    Vector2 textSize = MeasureTextEx(game_state->font, score_one_text, FONT_SIZE, 0);
+                    DrawRectangleLines(score_one_position.x, score_one_position.y, textSize.x, textSize.y, ORANGE);
+                }
+            }
+
+            { // Draw player two score
+                const Vector2 score_two_position = (Vector2) { vw(60), vh(5) };
+                const char *score_two_text = TextFormat("%i", game_state->player_two_score);
+                DrawTextEx(game_state->font, score_two_text, score_two_position, FONT_SIZE, 0, WHITE);
+                
+                // Draw player two score outline
+                if (game_state->debug_mode) {
+                    Vector2 textSize = MeasureTextEx(game_state->font, score_two_text, FONT_SIZE, 0);
+                    DrawRectangleLines(score_two_position.x, score_two_position.y, textSize.x, textSize.y, ORANGE);
+                }
+            }
 
             // Draw ball
             DrawRectangleV(ball->position, (Vector2) { 25, 25 }, WHITE);
@@ -220,6 +239,14 @@ int main(void)
 
     CloseAudioDevice();
     CloseWindow();
+
+    { // Cleanup
+        UnloadFont(game_state->font);
+        free(game_state);
+        free(ball);
+        free(paddle_one);
+        free(paddle_two);
+    }
 
     return 0;
 }
